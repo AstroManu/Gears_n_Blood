86d66d6ce910dd3dc2d437c0617cc0880c500869
Manuel <manuel.bergeron.d@gmail.com>
2018-02-22 19:35:53 -0500

    Units + anim : seeker et Marine

---
+++ Assets/AnimatorDebug.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorDebug : MonoBehaviour {
+
+    //Fetch the Animator
+    Animator m_Animator;
+
+
+	// Use this for initialization
+	void Start () {
+		m_Animator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	    if (Input.GetKey("m"))
+			m_Animator.SetBool("Moving", true);
+		else
+			m_Animator.SetBool("Moving", false);
+
+
+	    if (Input.GetKey("n"))
+			m_Animator.SetBool("Attacking", true);
+		else
+			m_Animator.SetBool("Attacking", false);
+
+
+		if (Input.GetKey("k"))
+			m_Animator.SetBool("Dying", true);
+		else
+			m_Animator.SetBool("Dying", false);
+	}
+}
