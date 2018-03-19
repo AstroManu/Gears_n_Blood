51e2281cc8035fe6ce6ae271b5e98234c3f2252c
Unknown <harvey.alexis@hotmail.com>
2018-03-19 19:20:31 -0400

    Ability: teleport

---
+++ Assets/Scripts/Abilities/Ab_Effects/AbE_TeleportCaster.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "TeleportCaster", menuName = "Abilities/Effects/Teleport caster", order = 37)]
+public class AbE_TeleportCaster : AbilityEffect {
+
+	public override void DoEffect (GameUnit caster, Vector3 target, LayerMask canHit, EventCaster eventCaster)
+	{
+		caster.agent.Warp (eventCaster.transform.position);
+		caster.controller.worldTarget = eventCaster.transform.position;
+	}
+
+}
