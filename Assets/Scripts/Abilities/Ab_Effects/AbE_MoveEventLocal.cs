7d610dabe1bd7da546f50ff9ce17027f2c36d671
Unknown <harvey.alexis@hotmail.com>
2018-04-01 00:20:03 -0400

    Event Translate
    AbE_MoveEventLocal + AbE_MoveEventWorld. J'ai quelques améliorations pour lesquels ça va servir. Aussi, j'ai changé la description du flametrower et du mortar pour quelque chose d'un peu plus pertinent.

---
+++ Assets/Scripts/Abilities/Ab_Effects/AbE_MoveEventLocal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "MoveEventLocal_Template", menuName = "Abilities/Effects/MoveEvent local", order = 37)]
+public class AbE_MoveEventLocal : AbilityEffect {
+
+	public Vector3 localTranslate;
+
+	public override void DoEffect (GameUnit caster, Vector3 target, LayerMask canHit, EventCaster eventCaster)
+	{
+		eventCaster.transform.Translate (localTranslate, Space.Self);
+	}
+
+}
---
+++ Assets/Scripts/Abilities/Ab_Effects/AbE_MoveEventWorld.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "MoveEventWorld_Template", menuName = "Abilities/Effects/MoveEvent world", order = 37)]
+public class AbE_MoveEventWorld : AbilityEffect {
+
+	public Vector3 worldTranslate;
+
+	public override void DoEffect (GameUnit caster, Vector3 target, LayerMask canHit, EventCaster eventCaster)
+	{
+		eventCaster.transform.Translate (worldTranslate, Space.Self);
+	}
+
+}
