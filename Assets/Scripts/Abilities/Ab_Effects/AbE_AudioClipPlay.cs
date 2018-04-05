54d734e730e0e07e420ea3afd851cb5cf9802b26
Unknown <harvey.alexis@hotmail.com>
2018-04-05 15:56:58 -0400

    AbE_AudioClipPlay

---
+++ Assets/Scripts/Abilities/Ab_Effects/AbE_AudioClipPlay.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu (fileName = "AudioClipPlay", menuName = "Abilities/Effects/AudioClip Play", order = 37)]
+public class AbE_AudioClipPlay : AbilityEffect {
+
+	[Tooltip ("An AudioClip that is played when the event start")] public AudioClip soundFx;
+
+	public override void DoEffect (GameUnit caster, Vector3 target, LayerMask canHit, EventCaster eventCaster)
+	{
+		eventCaster.aS.PlayOneShot (soundFx);
+	}
+
+}
