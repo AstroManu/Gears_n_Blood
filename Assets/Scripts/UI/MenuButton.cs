06e0e14d807c1f4f2483599a9baa785818ab3a67
Alexis <harvey.alexis@hotmail.com>
2018-04-22 19:09:11 -0400

    MainMenu Fonctionnel

---
+++ Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuButton : MonoBehaviour {
+
+	public bool quitGame;
+	public string sceneToLoad;
+
+	public Sprite selectedSprite;
+	public Sprite unselectedSprite;
+	public Sprite activatedSprite;
+
+	private Image img;
+
+	void Awake ()
+	{
+		img = GetComponent<Image> ();
+		DeselectButton ();
+	}
+
+	public void SelectButton ()
+	{
+		img.sprite = selectedSprite;
+	}
+
+	public void DeselectButton ()
+	{
+		img.sprite = unselectedSprite;
+	}
+
+	public void ActivateButton ()
+	{
+		img.sprite = activatedSprite;
+		if (quitGame)
+		{
+			Application.Quit ();
+		}
+		else
+		{
+			SceneManager.LoadScene (sceneToLoad);
+		}
+	}
+}
---
+++ Assets/Scripts/UI/MenuManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+
+public class MenuManager : MonoBehaviour {
+
+	public string playerName = "Player1";
+	private Player player;
+
+	public MenuButton[] buttons;
+	[HideInInspector] public int currentSelect = 0;
+
+	void Start ()
+	{
+		buttons [0].SelectButton ();
+		player = ReInput.players.GetPlayer (playerName);
+	}
+
+	void Update ()
+	{
+		if (player.GetButtonDown ("Next"))
+		{
+			buttons [currentSelect].DeselectButton ();
+			currentSelect++;
+			if (currentSelect >= buttons.Length)
+			{
+				currentSelect = 0;
+			}
+			buttons [currentSelect].SelectButton ();
+		}
+
+		if (player.GetButtonDown ("Previous"))
+		{
+			buttons [currentSelect].DeselectButton ();
+			currentSelect--;
+			if (currentSelect < 0)
+			{
+				currentSelect = buttons.Length - 1;
+			}
+			buttons [currentSelect].SelectButton ();
+		}
+
+		if (player.GetButtonDown ("Squad1"))
+		{
+			buttons [currentSelect].ActivateButton ();
+		}
+		if (player.GetButtonDown ("Squad2"))
+		{
+			currentSelect = buttons.Length - 1;
+			buttons [currentSelect].SelectButton ();
+		}
+	}
+
+}
